1abff5d8e2952cae41dac065be7e146751bb8eaf
苏财泉 <546726352@qq.com>
2019-08-17 22:13:53 +0800

    test

--- app/router.ts
+++ app/router.ts
@@ -2,19 +2,16 @@ import {Application} from 'egg';
 import * as wechat from 'co-wechat';
 
 export default (app: Application) => {
-  const {controller, config, router} = app;
-  app.logger.info(config);
-  router.use(async (ctx, next) => {
+  app.use(async (ctx, next) => {
     ctx.logger.info(`method:${ctx.method} path:${ctx.path} ${JSON.stringify(ctx.queries)}`);
     await next();
     ctx.logger.info(ctx.body);
   });
-  router.use(wechat({
+  app.use(wechat({
     token: process.env.WX_TOKEN,
     appid: process.env.WX_APPID,
     encodingAESKey: process.env.ENCODING_AES_KEY,
   }).middleware(async message => {
     return message;
   }));
-  router.get('/', controller.home.index);
 };
