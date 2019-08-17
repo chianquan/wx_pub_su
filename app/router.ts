84321a188fef3545aebcb205c9ce24730352448f
苏财泉 <546726352@qq.com>
2019-08-17 18:47:24 +0800

    use co-wechat package

--- app/router.ts
+++ app/router.ts
@@ -1,7 +1,16 @@
-import { Application } from 'egg';
+import {Application} from 'egg';
+import * as wechat from 'co-wechat';
 
 export default (app: Application) => {
-  const { controller, router } = app;
+  const {controller, router} = app;
 
+  const config = {
+    token: process.env.WX_TOKEN,
+    appid: process.env.WX_APPID,
+    encodingAESKey: process.env.ENCODING_AES_KEY,
+  };
+  router.use(wechat(config).middleware(async message => {
+    return message;
+  }));
   router.get('/', controller.home.index);
 };
