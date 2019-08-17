77ce6ab55183b4fa124bd2701bd530de8b620dd7
苏财泉 <546726352@qq.com>
2019-08-18 01:24:53 +0800

    线上测试

--- app/router.ts
+++ app/router.ts
@@ -2,6 +2,9 @@ import {Application} from 'egg';
 import * as wechat from 'co-wechat';
 
 export default (app: Application) => {
+  // app.use(async (ctx: Context) => {
+  //   ctx.body = await ctx.service.guessStock.guess(0.98);
+  // });
   app.use(
     wechat({
       token: process.env.WX_TOKEN,
@@ -9,6 +12,18 @@ export default (app: Application) => {
       encodingAESKey: process.env.ENCODING_AES_KEY,
     }).middleware(async (message, ctx) => {
       ctx.logger.info(message);
+      if (message.MsgType === 'text') {
+        const content: string = message.Content;
+        if (content.includes('猜涨跌')) {
+
+          const tmp = /[\d.]+/.exec(content);
+          const percentStr = tmp && tmp[0] && parseFloat(tmp[0]);
+          if (!percentStr) {
+            return ctx.service.guessStock.guess();
+          }
+          return ctx.service.guessStock.guess(percentStr / 100);
+        }
+      }
       return message;
     }),
   );
